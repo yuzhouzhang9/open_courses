650983908622473d7952b05f451836210ec13861
Yu Zhang <yuzhouzhang9@163.com>
2024-10-25 12:14:05 +0000

    implement data.cpp

--- cmu15-418/asst1/prog4_sqrt/data.cpp
+++ cmu15-418/asst1/prog4_sqrt/data.cpp
@@ -1,8 +1,9 @@
 #include <algorithm>
 
 // Generate random data
-void initRandom(float *values, int N) {
-    for (int i=0; i<N; i++)
+void initRandom(float *values, int N)
+{
+    for (int i = 0; i < N; i++)
     {
         // random input values
         values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
@@ -10,20 +11,34 @@ void initRandom(float *values, int N) {
 }
 
 // Generate data that gives high relative speedup
-void initGood(float *values, int N) {
-    for (int i=0; i<N; i++)
+// 256 / 32
+void initGood(float *values, int N)
+{
+    for (int i = 0; i < N; i++)
     {
         // Todo: Choose values
-        values[i] = 1.0f;
+        // values[i] = 1.0f;
+        // values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // generate data equte random input values
+        // if(i&1)
+            values[i] = .001f + 2.998f / 8 * static_cast<float>(rand()) / RAND_MAX + 2.998 / 8 * 7;
+        // else
+            // values[i] = .001f + 2.998f / 8 * static_cast<float>(rand()) / RAND_MAX + 2.998 / 8 ;
     }
 }
 
 // Generate data that gives low relative speedup
-void initBad(float *values, int N) {
-    for (int i=0; i<N; i++)
+void initBad(float *values, int N)
+{
+    for (int i = 0; i < N; i++)
     {
         // Todo: Choose values
-        values[i] = 1.0f;
+        // values[i] = 1.0f;
+        // values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // generate data equte random input values
+        if(i&1)
+            values[i] = .001f + 2.998f / 8 * static_cast<float>(rand()) / RAND_MAX + 2.998 / 8 * 7;
+        else
+            values[i] = .001f + 2.998f / 8 * static_cast<float>(rand()) / RAND_MAX + 2.998 / 8 ;
     }
 }
-
--- cmu15-418/asst1/prog4_sqrt/main.cpp
+++ cmu15-418/asst1/prog4_sqrt/main.cpp
@@ -3,44 +3,57 @@
 #include <pthread.h>
 #include <math.h>
 #include <getopt.h>
-
+#include <iostream>
+#include <cassert>
 #include "CycleTimer.h"
 #include "sqrt_ispc.h"
 
 using namespace ispc;
 
-typedef enum { DATA_RANDOM, DATA_GOOD, DATA_BAD } data_t;
+typedef enum
+{
+    DATA_RANDOM,
+    DATA_GOOD,
+    DATA_BAD
+} data_t;
 
-extern void sqrtSerial(int N, float startGuess, float* values, float* output);
+extern void sqrtSerial(int N, float startGuess, float *values, float *output);
 
 extern void initRandom(float *values, int N);
 extern void initGood(float *values, int N);
 extern void initBad(float *values, int N);
 
-void usage(const char* progname) {
+void usage(const char *progname)
+{
     printf("Usage: %s [options]\n", progname);
     printf("Program Options:\n");
     printf("  -d  --data r|g|b   Initialize with random|good|bad data\n");
     printf("  -?  --help         This message\n");
 }
 
-
-static void verifyResult(int N, float* result, float* gold) {
-    for (int i=0; i<N; i++) {
-        if (fabs(result[i] - gold[i]) > 1e-4) {
+static void verifyResult(int N, float *result, float *gold)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (fabs(result[i] - gold[i]) > 1e-4)
+        {
             printf("Error: [%d] Got %f expected %f\n", i, result[i], gold[i]);
         }
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     const int N = 20 * 1000 * 1000;
+    // std::cout<< N%64<<"\n";
+    // 数据对齐
+    assert(N%64 == 0);
     const float initialGuess = 1.0f;
 
-    float* values = new float[N];
-    float* output = new float[N];
-    float* gold = new float[N];
+    float *values = new float[N];
+    float *output = new float[N];
+    float *gold = new float[N];
 
     data_t dmode = DATA_RANDOM;
 
@@ -48,55 +61,59 @@ int main(int argc, char *argv[]) {
     int opt;
     static struct option long_options[] = {
         {"data", 1, 0, 'd'},
-        {"help",  0, 0, '?'},
-        {0 ,0, 0, 0}
-    };
+        {"help", 0, 0, '?'},
+        {0, 0, 0, 0}};
 
-    while ((opt = getopt_long(argc, argv, "d:?", long_options, NULL)) != EOF) {
-        switch (opt) {
+    while ((opt = getopt_long(argc, argv, "d:?", long_options, NULL)) != EOF)
+    {
+        switch (opt)
+        {
         case 'd':
-	    if (strlen(optarg) != 1) {
-		fprintf(stderr, "Invalid data option '%s'\n", optarg);
-		return 1;
-	    }
-	    switch (optarg[0]) {
-	    case 'r':
-		dmode = DATA_RANDOM;
-		break;
-	    case 'g':
-		dmode = DATA_GOOD;
-		break;
-	    case 'b':
-		dmode = DATA_BAD;
-		break;
-	    default:
-		fprintf(stderr, "Invalid data option '%s'\n", optarg);
-		return 1;
-	    }
-	    break;
+            if (strlen(optarg) != 1)
+            {
+                fprintf(stderr, "Invalid data option '%s'\n", optarg);
+                return 1;
+            }
+            switch (optarg[0])
+            {
+            case 'r':
+                dmode = DATA_RANDOM;
+                break;
+            case 'g':
+                dmode = DATA_GOOD;
+                break;
+            case 'b':
+                dmode = DATA_BAD;
+                break;
+            default:
+                fprintf(stderr, "Invalid data option '%s'\n", optarg);
+                return 1;
+            }
+            break;
         case '?':
         default:
             usage(argv[0]);
             return 0;
         }
     }
-    switch (dmode) {
+    switch (dmode)
+    {
     case DATA_RANDOM:
-	initRandom(values, N);
-	break;
+        initRandom(values, N);
+        break;
     case DATA_GOOD:
-	initGood(values, N);
-	break;
+        initGood(values, N);
+        break;
     case DATA_BAD:
-	initBad(values, N);
-	break;
+        initBad(values, N);
+        break;
     default:
-	fprintf(stderr, "Error.  Unknown data mode %d\n", dmode);
-	return 1;
+        fprintf(stderr, "Error.  Unknown data mode %d\n", dmode);
+        return 1;
     }
 
     // generate a gold version to check results
-    for (int i=0; i<N; i++)
+    for (int i = 0; i < N; i++)
         gold[i] = sqrt(values[i]);
 
     //
@@ -104,7 +121,8 @@ int main(int argc, char *argv[]) {
     // minimum time.
     //
     double minSerial = 1e30;
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 3; ++i)
+    {
         double startTime = CycleTimer::currentSeconds();
         sqrtSerial(N, initialGuess, values, output);
         double endTime = CycleTimer::currentSeconds();
@@ -120,7 +138,8 @@ int main(int argc, char *argv[]) {
     // time of three runs.
     //
     double minISPC = 1e30;
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 3; ++i)
+    {
         double startTime = CycleTimer::currentSeconds();
         sqrt_ispc(N, initialGuess, values, output);
         double endTime = CycleTimer::currentSeconds();
@@ -139,7 +158,8 @@ int main(int argc, char *argv[]) {
     // Tasking version of the ISPC code
     //
     double minTaskISPC = 1e30;
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 3; ++i)
+    {
         double startTime = CycleTimer::currentSeconds();
         sqrt_ispc_withtasks(N, initialGuess, values, output);
         double endTime = CycleTimer::currentSeconds();
@@ -150,8 +170,8 @@ int main(int argc, char *argv[]) {
 
     verifyResult(N, output, gold);
 
-    printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial/minISPC);
-    printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial/minTaskISPC);
+    printf("\t\t\t\t(%.2fx speedup from ISPC)\n", minSerial / minISPC);
+    printf("\t\t\t\t(%.2fx speedup from task ISPC)\n", minSerial / minTaskISPC);
 
     delete[] values;
     delete[] output;
